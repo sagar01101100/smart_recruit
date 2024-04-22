6644adc4bd120467338de69be4ab96feab8ccb15
sagar01101100 <sagar01101100@gmail.com>
2024-04-22 11:52:56 +0530

    Database updated

--- constants/db_config.php
+++ constants/db_config.php
@@ -1,6 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "job_portal";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "job_portal";
+
+//Online Database:
+$servername = "http://sql6.freesqldatabase.com";
+$username = "sql6700184";
+$password = "TzvzeFbWub";
+$dbname = "sql6700184";
+
 ?>
